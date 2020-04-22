def827e62779987c0016ee36b5d592a741932617
prostochlen <iwan.maksackov@yandex.ru>
2020-04-22 21:03:13 +0300

    History commit

--- js/scroll.js
+++ js/scroll.js
@@ -4,10 +4,12 @@ $(window).on("scroll", function () {
         $('.headerMain').addClass('scrolled');
         $('.item').addClass('scrolled');
         $('.main-title').addClass('scrolled');
+        $(".logo").attr("src", "/img/logo-black.png");
     }   
     if( scrolled <= 107 ) {     
         $('.headerMain').removeClass('scrolled');
         $('.item').removeClass('scrolled');
         $('.main-title').removeClass('scrolled');
+        $(".logo").attr("src", "/img/logo.png");
     }
 });
