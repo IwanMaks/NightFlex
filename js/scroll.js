7c686a2a41b6573b5d3c2dcd489e81ff327198f3
prostochlen <iwan.maksackov@yandex.ru>
2020-04-26 01:34:50 +0300

    inside commit

--- js/scroll.js
+++ js/scroll.js
@@ -4,12 +4,12 @@ $(window).on("scroll", function () {
         $('.headerMain').addClass('scrolled');
         $('.item').addClass('scrolled');
         $('.main-title').addClass('scrolled');
-        $(".logo").attr("src", "img/logo-black.png");
+        $(".logo").attr("src", "img/logo/logo-black.png");
     }   
     if( scrolled <= 107 ) {     
         $('.headerMain').removeClass('scrolled');
         $('.item').removeClass('scrolled');
         $('.main-title').removeClass('scrolled');
-        $(".logo").attr("src", "img/logo.png");
+        $(".logo").attr("src", "img/logo/logo.png");
     }
 });
